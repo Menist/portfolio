2ef76177b7a98f7f0ff9972711af727e564ba84b
Mark <cssincubator@gmail.com>
2024-04-15 13:57:48 +0300

    refactor code, add map

--- src/layout/section/education/Education.tsx
+++ src/layout/section/education/Education.tsx
@@ -4,33 +4,45 @@ import {EducationStyled} from "./EducationStyled";
 import {BlockTitle} from "../../../components/SectionTitle/BlockTitle";
 import {Fade} from "react-awesome-reveal";
 
+const itemsData = [
+    {
+        infoTitle: "University of Toronto",
+        infoSubTitle: "Student",
+        infoData: "Jan 1016 - Dec 2021",
+        descriptionTitle: "Certificate of web training",
+        descriptionText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet dapibus nibh ut faucibus nunc, egestas id amet porttitor. Pulvinar quisque sed amet, nulla nunc. Eleifend sodales posuere fusce tempus etiam et pellentesque. Molestie risus enim neque eget dui."
+    },
+    {
+        infoTitle: "Programming Course",
+        infoSubTitle: "Student",
+        infoData: "Jan 1016 - Dec 2021",
+        descriptionTitle: "Certificate of web training",
+        descriptionText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet dapibus nibh ut faucibus nunc, egestas id amet porttitor. Pulvinar quisque sed amet, nulla nunc. Eleifend sodales posuere fusce tempus etiam et pellentesque. Molestie risus enim neque eget dui."
+    },
+    {
+        infoTitle: "Web developer courses",
+        infoSubTitle: "Student",
+        infoData: "Jan 1016 - Dec 2021",
+        descriptionTitle: "Certificate of web training",
+        descriptionText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet dapibus nibh ut faucibus nunc, egestas id amet porttitor. Pulvinar quisque sed amet, nulla nunc. Eleifend sodales posuere fusce tempus etiam et pellentesque. Molestie risus enim neque eget dui."
+    }
+];
+
 export const Education = () => {
     return (
         <Fade cascade damping={.9}>
-        <EducationSection id={'education'}>
-            <BlockTitle title={'Education'} subTitle={'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. lorem ipsum'}></BlockTitle>
-                <EducationStyled
-                    firstTitle={"University of Toronto"}
-                    subTitle={"Student"}
-                    data={"Jan 1016 - Dec 2021"}
-                    secondTitle={"Certificate of web training"}
-                    text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet dapibus nibh ut faucibus nunc, egestas id amet porttitor. Pulvinar quisque sed amet, nulla nunc. Eleifend sodales posuere fusce tempus etiam et pellentesque. Molestie risus enim neque eget dui."}/>
-            <EducationStyled
-                firstTitle={"Programming Course"}
-                subTitle={"Student"}
-                data={"Jan 1016 - Dec 2021"}
-                secondTitle={"Certificate of web training"}
-                text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet dapibus nibh ut faucibus nunc, egestas id amet porttitor. Pulvinar quisque sed amet, nulla nunc. Eleifend sodales posuere fusce tempus etiam et pellentesque. Molestie risus enim neque eget dui."}/>
-            <EducationStyled
-                firstTitle={"Web developer courses"}
-                subTitle={"Student"}
-                data={"Jan 1016 - Dec 2021"}
-                secondTitle={"Certificate of web training"}
-                text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet dapibus nibh ut faucibus nunc, egestas id amet porttitor. Pulvinar quisque sed amet, nulla nunc. Eleifend sodales posuere fusce tempus etiam et pellentesque. Molestie risus enim neque eget dui."}/>
-        </EducationSection>
-         </Fade>
-    );
+            <EducationSection id={'education'}>
+                <BlockTitle title={'Education'}
+                            subTitle={'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. lorem ipsum'}></BlockTitle>
+
+                {itemsData.map((item, index)=>{
+                    return <EducationStyled key={index} infoTitle={item.infoTitle} infoSubTitle={item.infoSubTitle} infoData={item.infoData} descriptionTitle={item.descriptionTitle} descriptionText={item.descriptionText}/>
+                })};
+
+                </EducationSection>
+        </Fade>
+);
 };
 const EducationSection = styled.section`
-    
+
 `
--- src/layout/section/education/EducationStyled.tsx
+++ src/layout/section/education/EducationStyled.tsx
@@ -7,11 +7,11 @@ import {ThemeObjectType} from "../../../styles/ColorSheme";
 
 
 type EducationStyledComponentPropsType = {
-    firstTitle: string
-    subTitle: string
-    data: string
-    secondTitle: string
-    text: string
+    infoTitle: string
+    infoSubTitle: string
+    infoData: string
+    descriptionTitle: string
+    descriptionText: string
 }
 export const EducationStyled = (props: EducationStyledComponentPropsType) => {
     const {themeObj} = useTheme();
@@ -21,13 +21,13 @@ export const EducationStyled = (props: EducationStyledComponentPropsType) => {
             <Fade cascade damping={.5}>
                 <FlexWrapper align={'flex-start'} gap={"12%"}>
                     <FlexWrapper flex={"0 1 25%"} gap={"25px"}>
-                        <FirstTitleEducation themeObj={themeObj}>{props.firstTitle}</FirstTitleEducation>
-                        <SubTitleTitleEducation>{props.subTitle}</SubTitleTitleEducation>
-                        <EducationData themeObj={themeObj}>{props.data}</EducationData>
+                        <EducationInfoTitle themeObj={themeObj}>{props.infoTitle}</EducationInfoTitle>
+                        <EducationInfoSubTitle>{props.infoSubTitle}</EducationInfoSubTitle>
+                        <EducationInfoData themeObj={themeObj}>{props.infoData}</EducationInfoData>
                     </FlexWrapper>
                     <FlexWrapper flex={"0 1 63%"} gap={"20px 0"}>
-                        <SecondTitleEducation themeObj={themeObj}>{props.secondTitle}</SecondTitleEducation>
-                        <TextEducation>{props.text}</TextEducation>
+                        <EducationDescriptionTitle themeObj={themeObj}>{props.descriptionTitle}</EducationDescriptionTitle>
+                        <EducationDescriptionText>{props.descriptionText}</EducationDescriptionText>
                     </FlexWrapper>
                 </FlexWrapper>
             </Fade>
@@ -69,17 +69,17 @@ const EducationStyledComponent = styled.div<{themeObj: ThemeObjectType}>`
   }
 `
 
-const FirstTitleEducation = styled.h4<{themeObj: ThemeObjectType}>`
+const EducationInfoTitle = styled.h4<{themeObj: ThemeObjectType}>`
   width: 100%;
   font-weight: 500;
   text-transform: capitalize;
   color:${({themeObj}) =>themeObj.colors.headerText};
   gap: 25px;
 `
-const SubTitleTitleEducation = styled.span`
+const EducationInfoSubTitle = styled.span`
   font-weight: 500;
 `
-const EducationData = styled.span<{themeObj: ThemeObjectType}>`
+const EducationInfoData = styled.span<{themeObj: ThemeObjectType}>`
   font-weight: 400;
   font-size: 10px;
   background-color: ${({themeObj}) =>themeObj.colors.backgroundColorSection};
@@ -87,8 +87,8 @@ const EducationData = styled.span<{themeObj: ThemeObjectType}>`
   background-color: ${({themeObj}) =>themeObj.colors.primary};
   padding: 5px 10px;
 `
-const SecondTitleEducation = styled(FirstTitleEducation)``
-const TextEducation = styled.div`
+const EducationDescriptionTitle = styled(EducationInfoTitle)``
+const EducationDescriptionText = styled.div`
   padding-bottom: 30px;
   max-width: 720px;
   line-height: 24px;
