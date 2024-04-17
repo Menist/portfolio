1a6a93ccf180ec52188eae75a51c0d900088c8fe
Mark <cssincubator@gmail.com>
2024-04-17 21:47:03 +0300

    converted from flex to grid

--- src/layout/section/services/Services.tsx
+++ src/layout/section/services/Services.tsx
@@ -1,63 +1,70 @@
-//отрисовка всей секции My services
+// Services.js
 import React from 'react';
 import styled from "styled-components";
-import {ServicesCard} from "./card/ServicesCard";
-import {BlockTitle} from "../../../components/SectionTitle/BlockTitle";
-import {Fade} from "react-awesome-reveal";
-import {useTheme} from "../../../ThemeProvider";
+import { ServicesCard } from "./card/ServicesCard";
+import { BlockTitle } from "../../../components/SectionTitle/BlockTitle";
+import { Fade } from "react-awesome-reveal";
+import { useTheme } from "../../../ThemeProvider";
+
+const servicesData = [
+    { iconId: "webDevelopment",
+        title: "web development",
+        text: "blog, e-commerce",
+        link: ''
+    },
+    { iconId: "uIuxDesign",
+        title: "uI/uX design",
+        text: "Mobile app, website design",
+        link: ''
+    },
+    { iconId: "soundDesign",
+        title: "sound design",
+        text: "Voice Over, Beat Making",
+        link: ''
+    },
+    { iconId: "gameDesign",
+        title: "game design",
+        text: "Character Design, Props & Objects",
+        link: ''
+    },
+    { iconId: "photography",
+        title: "photography",
+        text: "portrait, product photography",
+        link: ''
+    },
+    { iconId: "",
+        title: "advertising",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae nulla diam in ac dictum a urna viverra morbi.",
+        link: 'order now'
+    }
+];
 
 export const Services = () => {
-    const {themeObj } = useTheme();
+    const { themeObj } = useTheme();
     return (
-        <Fade cascade damping={.9}>
+        <Fade cascade damping={0.9}>
             <ServicesSection theme={themeObj} id={'services'}>
-                <BlockTitle title={"my services"}
-                            subTitle={"Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. lorem ipsum"}></BlockTitle>
+                <BlockTitle title={"my services"} subTitle={"Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. lorem ipsum"} />
                 <Container>
-                    <ServicesCard
-                        iconId={"webDevelopment"}
-                        title={"web development"}
-                        text={"blog, e-commerce"}
-                        link={''}
-                    />
-                    <ServicesCard
-                        iconId={"uIuxDesign"}
-                        title={"uI/uX design"}
-                        text={"Mobile app, website design"}
-                        link={''}
-                    />
-                    <ServicesCard
-                        iconId={"soundDesign"}
-                        title={"sound design"}
-                        text={"Voice Over, Beat Making"}
-                        link={''}
-                    />
-                    <ServicesCard
-                        iconId={"gameDesign\n"}
-                        title={"game design"}
-                        text={"Character Design, Props & Objects"}
-                        link={''}
-                    />
-                    <ServicesCard
-                        iconId={"photography"}
-                        title={"photography"}
-                        text={"portrait, product photography"}
-                        link={''}
-                    />
-                    <ServicesCard
-                        iconId={""}
-                        title={"advertising"}
-                        text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae nulla diam in ac dictum a urna viverra morbi."}
-                        link={'order now'}
-                    />
+                    {servicesData.map(service => (
+                        <ServicesCard
+                            key={service.title} // Уникальный ключ для каждой карточки
+                            iconId={service.iconId}
+                            title={service.title}
+                            text={service.text}
+                            link={service.link}
+                        />
+                    ))}
                 </Container>
             </ServicesSection>
         </Fade>
     );
 };
+
 const ServicesSection = styled.section`
   text-align: center;
-`
+`;
+
 const Container = styled.div`
   display: grid;
   width: 100%;
@@ -68,4 +75,5 @@ const Container = styled.div`
   @media screen and (max-width: 969px) {
     justify-items: center;
   }
-`
+`;
+
