c1db28c520ba450108214c8868af5d2a952451d7
Mark <cssincubator@gmail.com>
2024-04-20 09:10:55 +0300

    add favicons

--- src/layout/sideBar/section/extraSkills/SidebarExtraSkillsFragment.tsx
+++ src/layout/sideBar/section/extraSkills/SidebarExtraSkillsFragment.tsx
@@ -2,15 +2,16 @@ import React from 'react';
 import styled from "styled-components";
 import {Icon} from "../../../../components/icon/Icon";
 import {FlexWrapper} from "../../../../components/FlexWrapper";
-
+import {useTheme} from "../../../../ThemeProvider";
 type ExtraSkillsFragmentPropsType = {
     text: string
 }
 export const SidebarExtraSkillsFragment = (props: ExtraSkillsFragmentPropsType) => {
+    const {themeobj} = useTheme();
     return (
         <SidebarExtraSkills>
             <FlexWrapper align={"center"} gap={"15px"}>
-                <Icon iconId={"squareSidebar"} height={15} width={15}/>
+                <Icon iconId={"squareSidebar"} color={themeobj.colors.primary} fill={themeobj.colors.backgroundColorSection} height={15} width={15}/>
                 <SidebarExtraSkillsText>{props.text}</SidebarExtraSkillsText>
             </FlexWrapper>
         </SidebarExtraSkills>
