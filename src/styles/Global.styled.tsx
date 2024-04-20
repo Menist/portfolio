9db1ba0c493cd11967cd726857829aa4b40e98c7
Mark <cssincubator@gmail.com>
2024-04-20 12:34:23 +0300

    add svg and refactor ways to svg

--- src/styles/Global.styled.tsx
+++ src/styles/Global.styled.tsx
@@ -1,5 +1,6 @@
 import {ThemeProvider} from "../ThemeProvider";
 import {createGlobalStyle} from "styled-components";
+import {themeobjectType} from "./ColorSheme";
 
 export const GlobalStyle = ({children}: any) => {
     return (
@@ -9,7 +10,7 @@ export const GlobalStyle = ({children}: any) => {
     );
 };
 
-export const GlobalStyled = createGlobalStyle<{ themeobj: any }>`
+export const GlobalStyled = createGlobalStyle<{ themeobj: themeobjectType }>`
   *,
   *::before,
   *::after {
@@ -17,16 +18,10 @@ export const GlobalStyled = createGlobalStyle<{ themeobj: any }>`
     margin: 0;
     box-sizing: border-box;
   }
-
-  & {
-    scrollbar-width: thin;
-    scrollbar-color: ${({ themeobj }) => themeobj.colors.primary};
-  }
-
+  
   &::-webkit-scrollbar {
     width: 5px;
-    border:${({ themeobj }) => themeobj.colors.accent}
-    border-radius: 30px;
+    border: ${({ themeobj }) => themeobj.colors.accent} solid;
   }
 
   &::-webkit-scrollbar-thumb {
@@ -37,7 +32,7 @@ export const GlobalStyled = createGlobalStyle<{ themeobj: any }>`
   &::-webkit-scrollbar-thumb:hover {
     background-color: ${({ themeobj }) => themeobj.colors.accent};
   }
-
+  
   body {
     margin: 0;
     color: ${({ themeobj }) => themeobj.colors.mainText};
@@ -49,13 +44,12 @@ export const GlobalStyled = createGlobalStyle<{ themeobj: any }>`
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     font-size: ${({ themeobj }) => themeobj.text.textSize};
-    
 
     p {
       line-height: ${({ themeobj }) => themeobj.text.lineHeight};
     }
   }
-
+  
   a {
     text-decoration: none;
   }
@@ -72,27 +66,22 @@ export const GlobalStyled = createGlobalStyle<{ themeobj: any }>`
   h1, h2, h3, h4, h5, h6 {
     font-weight: bold;
     color: ${({ themeobj }) => themeobj.colors.headerText};
-    
   }
 
   h1 {
     font-size: calc((100vw - 375px) / (3000 - 375) * (75 - 30) + 30px);
-    //48px х 1440
   }
 
   h2 {
     font-size: calc((100vw - 375px) / (3000 - 375) * (40 - 27) + 27px);
-    //32px х 1440
   }
 
   h3 {
     font-size: calc((100vw - 375px) / (3000 - 375) * (30 - 24) + 24px);
-    //24px х 1440
   }
 
   h4 {
     font-size: calc((100vw - 375px) / (3000 - 375) * (19 - 18) + 18px);
-    //18px х 1440
   }
 
   Header, Footer, section {
@@ -101,4 +90,5 @@ export const GlobalStyled = createGlobalStyle<{ themeobj: any }>`
       padding: 0 10px;
     }
   }
-`
+  
+`;
